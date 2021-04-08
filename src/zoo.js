decdc64396e2e8cf0afe102ec549022169643ddd
Bruno Mendes <mendessbruno@gmail.com>
2021-04-08 15:41:28 -0300

    resolvido problema de lint

--- src/zoo.js
+++ src/zoo.js
@@ -10,7 +10,7 @@ eslint no-unused-vars: [
 */
 
 const { animals } = require('./data');
-const data = require('./data');
+// const data = require('./data');
 
 function animalsByIds(...ids) {
   const arr = [];
