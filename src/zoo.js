a2c54a4fc3040ec5049afd0ee61d784c33ed99e1
Giovanni <giovannitadeu08@gmail.com>
2021-04-08 16:16:30 -0300

    [Requisito 4] - Feito

--- src/zoo.js
+++ src/zoo.js
@@ -28,7 +28,7 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return { ...personalInfo, ...associatedWith }
 }
 
 function isManager(id) {
