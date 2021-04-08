3361e9d7cb42eaecd4807c5b50b7d5c89b0c3242
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-04-08 17:54:17 -0300

    Add function entryCalculator

--- src/zoo.js
+++ src/zoo.js
@@ -9,7 +9,7 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals, employees } = require('./data');
+const { animals, employees, prices } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
@@ -67,7 +67,10 @@ function animalCount(species) {
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  if(entrants === undefined) {
+    return 0;
+  }
+  return Object.entries(entrants).reduce((acc, [key, value]) => acc += (prices[key] * value), 0);
 }
 
 function animalMap(options) {
