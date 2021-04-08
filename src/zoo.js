555f7cc89bf60841c032a89ec20735c8d0461fa9
Patrick Morais <ppternunes@gmail.com>
2021-04-08 14:40:42 -0300

    requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -36,7 +36,7 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((elem) => elem.managers.some((elem2) => elem2 === id))
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
