de846a1bcf6c333e847e2b82cf0f8ed813fdbc77
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-04-08 19:14:00 -0300

    requisito 5 feito

--- src/zoo.js
+++ src/zoo.js
@@ -31,9 +31,9 @@ function createEmployee(personalInfo, associatedWith) {
   return { ...personalInfo, ...associatedWith };
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  return employees.some((anyManager) => anyManager.managers.includes(id));
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -76,7 +76,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
