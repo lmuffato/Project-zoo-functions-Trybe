668445649385c0644890e48d894d4fadc473485e
Thiago <thiagodesouza.dev@gmail.com>
2021-04-08 20:19:39 -0300

    atividade 7, reduce e find

--- src/zoo.js
+++ src/zoo.js
@@ -25,28 +25,58 @@ function animalsOlderThan(animal, age) {
 function employeeByName(employeeName) {
   // seu código aqui
   /* 'Sem parâmetros, retorna um objeto vazio', */
-  if (!employeeName) return {}; 
+  if (!employeeName) return {};
   /* 'Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário', */
   /* 'Quando provido o último nome do funcionário, retorna o objeto do funcionário' */
-  return data.employees.find(({firstName, lastName }) => 
-  firstName === employeeName || lastName === employeeName);
+  return data.employees.find(({ firstName, lastName }) =>
+    firstName === employeeName || lastName === employeeName);
 }
 
-function createEmployee(personalInfo, associatedWith) {
+const createEmployee = (personalInfo,
+  associatedWith) => ({ ...personalInfo, ...associatedWith });/* ;{
   // seu código aqui
-  
-}
+  return (personalInfo, associatedWith) => ({ ...personalInfo, ...associatedWith });
+  /* Cria um novo colaborador a partir de objetos contendo
+  informações pessoais, gerentes e animais gerenciados */
+/* const { id, firstName, lastName } = personalInfo;
+const { managers, responsibleFor } = associatedWith;
+const employee = {
+  id,
+  firstName,
+  lastName,
+  managers,
+  responsibleFor,
+};
+return employee; 
+} */
 
 function isManager(id) {
   // seu código aqui
+  /* 'Testa se o id passado é de um gerente' */
+  return data.employees.some(({ managers }) => managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   // seu código aqui
+  const employee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  data.employees.push(employee);
 }
 
 function animalCount(species) {
   // seu código aqui
+  if (species === undefined) {
+    return data.animals.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  return data.animals.find(({ name }) => name === species).residents.length;
 }
 
 function entryCalculator(entrants) {
