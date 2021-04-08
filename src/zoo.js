44cf944576eaec86d07d7ea60e0b9350ee18e117
Renzo Sevilha <sevilharenzo@gmail.com>
2021-04-08 13:48:41 -0300

    [REQUISITO 06]

--- src/zoo.js
+++ src/zoo.js
@@ -39,8 +39,14 @@ function isManager(id) {
   return employees.some((emplo) => emplo.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
