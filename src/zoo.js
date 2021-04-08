3d9f2b0dd9d2af42537d73361d266c83a89c8e93
Max Rudim <max.hermsdorff@gmail.com>
2021-04-08 14:52:50 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(ids) {
-  // seu código aqui
+  // seu código aquia
 }
 
 function animalsOlderThan(animal, age) {
