1adada61af2c3b12a93bc86073a7a2ecf2d287e8
Lucas André <lucas.cancado89@gmail.com>
2021-04-08 15:23:30 -0300

    Requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -27,12 +27,13 @@ function employeeByName(employeeName = '') {
   if (employeeName === '') {
     return {};
   }
-  return data.employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+  return data.employees.find((employee) => employee.firstName === employeeName
+  || employee.lastName === employeeName);
 }
 
-// function createEmployee(personalInfo, associatedWith) {
-//   // seu código aqui
-// }
+function createEmployee(personalInfo, associatedWith) {
+  return Object.assign(personalInfo, associatedWith);
+}
 
 // function isManager(id) {
 //   // seu código aqui
@@ -81,7 +82,7 @@ module.exports = {
   //   addEmployee,
   //   isManager,
   animalsOlderThan,
-//   oldestFromFirstSpecies,
-//   increasePrices,
-//   createEmployee,
+  //   oldestFromFirstSpecies,
+  //   increasePrices,
+  createEmployee,
 };
