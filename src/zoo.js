e77b3c388c20903360c859b9cc026659e4efa3ff
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-04-08 19:09:31 -0300

    requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -43,9 +43,9 @@ const createEmployee = (personalInfo, associatedWith) => ({ ...personalInfo, ...
 //   };
 //   return employee;
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  return data.employees.some((item) => item.managers.includes(id));
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -88,7 +88,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
