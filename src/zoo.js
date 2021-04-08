7d3361922e474ddc8ddd38698d3b8fdab1060dcd
Lucas Brito <lucasbrito.carneiro@gmail.com>
2021-04-08 18:38:49 -0300

    Big Commit 1 to 7

--- src/zoo.js
+++ src/zoo.js
@@ -9,34 +9,73 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');
+const { animals, employees } = require("./data");
+const data = require("./data");
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   // seu código aqui
+  return animals.filter((animal) => ids.includes(animal.id));
 }
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
+  return animals
+    .find((animalType) => animalType.name === animal)
+    .residents.every((animalAge) => animalAge.age >= age);
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find(
+    (employe) =>
+      employe.firstName === employeeName || employe.lastName === employeeName
+  );
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return (newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function isManager(id) {
   // seu código aqui
+  return employees.some((employe) =>
+    employe.managers.find((employeId) => employeId === id)
+  );
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers: managers || [],
+    responsibleFor: responsibleFor || [],
+  };
+
+  return employees.push(newEmployee);
 }
 
 function animalCount(species) {
   // seu código aqui
+  if (species === undefined) {
+    const obj = {};
+    animals.forEach((animal) => (obj[animal.name] = animal.residents.length));
+    return obj;
+  }
+  return animals.find((animal) => animal.name === species).residents.length;
 }
 
 function entryCalculator(entrants) {
