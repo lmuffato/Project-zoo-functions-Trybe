dfc9597c59660a6ad444330ddedcb6761944f061
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-04-08 17:28:57 -0300

    Requisito 1 - revisado(1)

--- src/zoo.js
+++ src/zoo.js
@@ -9,71 +9,64 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
   // seu código aqui
-
   if (!ids) {
     return [];
   }
-
   const animalsId = data.animals.filter((animal, index) => animal.id === ids[index]);
   return animalsId;
 }
-// console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
-// console.log(data.animals.forEach((animal) => {
-//   (animal.name === 'lions') ? animal : undefined;
-// }));
 
-function animalsOlderThan(animal, age) {
-  // seu código aqui
-}
+// function animalsOlderThan(animal, age) {
+//   // seu código aqui
+// }
 
-function employeeByName(employeeName) {
-  // seu código aqui
-}
+// function employeeByName(employeeName) {
+//   // seu código aqui
+// }
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+// function createEmployee(personalInfo, associatedWith) {
+//   // seu código aqui
+// }
 
-function isManager(id) {
-  // seu código aqui
-}
+// function isManager(id) {
+//   // seu código aqui
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   // seu código aqui
+// }
 
-function animalCount(species) {
-  // seu código aqui
-}
+// function animalCount(species) {
+//   // seu código aqui
+// }
 
-function entryCalculator(entrants) {
-  // seu código aqui
-}
+// function entryCalculator(entrants) {
+//   // seu código aqui
+// }
 
-function animalMap(options) {
-  // seu código aqui
-}
+// function animalMap(options) {
+//   // seu código aqui
+// }
 
-function schedule(dayName) {
-  // seu código aqui
-}
+// function schedule(dayName) {
+//   // seu código aqui
+// }
 
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+// function oldestFromFirstSpecies(id) {
+//   // seu código aqui
+// }
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+// function increasePrices(percentage) {
+//   // seu código aqui
+// }
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
+// function employeeCoverage(idOrName) {
+//   // seu código aqui
+// }
 
 module.exports = {
   entryCalculator,
