aba9b3cfb0dbadb02d75399de31f7e8b04ee9159
Viegas <mvrdgs@gmail.com>
2021-04-08 15:52:49 -0300

    requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -11,10 +11,11 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  
-  // seu código aqui
+function animalsByIds(...ids) {
+  return data.animals.filter((animal) => ids.some((id) => animal.id === id));
 }
+
+console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
