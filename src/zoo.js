5a1d5ae129385b42501b2fa2099f7787edbabfeb
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-04-08 19:45:05 -0300

    quarto requisito

--- src/zoo.js
+++ src/zoo.js
@@ -15,19 +15,27 @@ const { employees } = require('./data');
 function animalsByIds(...ids) {
   return animals.filter((animal) => ids.some((id) => id === animal.id));
 }
-// Lógica que o Rodrigo Luiz apresentou no plantão.
+// Tinha feito com dois forEach mas achei muito melhor a lógica que o Rodrigo Luiz apresentou no plantão, mais concisa.
 function animalsOlderThan(animal, age) {
   return animals.find((getAnimal) => getAnimal.name === animal)
     .residents.every(((animalAge) => animalAge.age >= age));
 }
 function employeeByName(employeeName) {
   if (typeof (employeeName) === 'undefined') return {};
-  const employeeFilter = employees.filter((employee) =>
-    employee.firstName === employeeName || employee.lastName === employeeName);
-  return employeeFilter.shift();
+  return employees.filter((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName).shift();
+  
 }
 
 function createEmployee(personalInfo, associatedWith) {
+  const newEmployee = { 
+    id: personalInfo.id, 
+    firstName: personalInfo.firstName,
+    lastName: personalInfo.lastName,
+    managers: associatedWith.managers,
+    responsibleFor: associatedWith.responsibleFor
+  }
+  return newEmployee
   // seu código aqui
 }
 
@@ -83,5 +91,24 @@ module.exports = {
   createEmployee,
 };
 
-// console.log(employeeByName('Wishart'));
+
+const param1 = {
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+};
+
+const param2 = {
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'
+  ],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+  ]
+};
+
+console.log(createEmployee(param1, param2))
 
