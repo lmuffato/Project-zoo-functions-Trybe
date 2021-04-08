9d9cfc1448aae4dbcf43ec5d2a00167efbc89cae
Gabriel Ferreira <glucas709@gmail.com>
2021-04-08 19:31:27 -0300

    Revert "Requisito 06 - productDetails correção"
    This reverts commit afd87a379f9861ea5a79c4fd91b79f4c462a565d.

--- src/zoo.js
+++ src/zoo.js
@@ -18,14 +18,9 @@ function animalsByIds(...ids) {
   return filterId(ids);
 }
 
-function animalsOlderThan(animal, ageInput) {
-  const animalFind = animals.find(({ name }) => animal === name);
-  const { residents } = animalFind;
-  const youngerThan = residents.filter(({ age }) => age < ageInput);
-  return youngerThan.length === 0;
+function animalsOlderThan(animal, age) {
+  // seu código aqui
 }
-
-console.log(animalsOlderThan('penguins', 10));
 
 function employeeByName(employeeName) {
   // seu código aqui
