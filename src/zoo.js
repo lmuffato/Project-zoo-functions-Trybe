7d33b9f49c33f98c001bce4a7d8855f5ffdc6504
Rafael Ribeiro <rafittu@outlook.com>
2021-04-08 18:00:08 -0300

    requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -43,9 +43,15 @@ function isManager(id) {
   return manager.managers.length < 2;
 }
 
-// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-//   // seu código aqui
-// }
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  data.employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
+}
 
 // function animalCount(species) {
 //   // seu código aqui
@@ -83,7 +89,7 @@ module.exports = {
   animalsByIds,
   employeeByName,
   // employeeCoverage,
-  // addEmployee,
+  addEmployee,
   isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
