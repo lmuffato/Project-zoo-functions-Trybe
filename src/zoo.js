c508a405037209ea129a12f9044857ceca6be886
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-04-08 15:51:21 -0300

    Requisito 1 - implementação da função animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,15 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  const { animals } = data;
+  return animals.filter((animal) => ids.includes(animal.id));
 }
+// console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
+//   'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+// console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+// console.log(data.animals[0].id);
+// console.log(data.animals);
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
