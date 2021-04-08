17b7978cabe082b2b0f119bf53360dbe73629913
Viegas <mvrdgs@gmail.com>
2021-04-08 15:59:52 -0300

    melhoria no codigo requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  return data.animals.filter((animal) => ids.some((id) => animal.id === id));
+  return data.animals.filter(({ id }) => ids.includes((id)));
 }
 
 console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
