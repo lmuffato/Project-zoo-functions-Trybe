826b408cfcd89f8526700fff2001af21476139d1
mallmann02 <leomallmann020302@gmail.com>
2021-04-08 17:12:42 -0300

    Requirements - 1

--- src/zoo.js
+++ src/zoo.js
@@ -9,14 +9,18 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  if (!ids) return [];
+  return animals.filter((animal) => {
+    return ids.includes(animal.id);
+  });
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  
 }
 
 function employeeByName(employeeName) {
