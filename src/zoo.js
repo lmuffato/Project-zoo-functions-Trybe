5471879acdac484b887a78c887e15baedf04b117
jonathan <jonathanfernandesjf98@gmail.com>
2021-04-08 16:59:41 -0300

    requisito 01 iniciando o projeto

--- src/zoo.js
+++ src/zoo.js
@@ -8,11 +8,15 @@ eslint no-unused-vars: [
   }
 ]
 */
-
+const { animals } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
+function animalsByIds(...ids) {
   // seu código aqui
+  if (ids.length === 0) {
+    return [];
+  }
+  return animals.filter((animal) => ids.some((id) => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
