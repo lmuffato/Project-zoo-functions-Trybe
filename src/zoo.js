551f43520f658bab11f84a99c825a3f02003a750
ThalitaC <thalitaceci@gmail.com>
2021-04-08 17:58:02 -0300

    renomeia elementos requisito 2

--- src/zoo.js
+++ src/zoo.js
@@ -13,7 +13,7 @@ const { animals } = require('./data');
 // const data = require('./data');
 
 // found about .includes at https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
-// Desenvolvido com ajuda das alunas Nathalia Zerbal, Debora Passos, Heloisa Hackenhaar, Djaniza Vasques e Bia Zidioti
+// Desenvolvido com ajuda das colegas Nathalia Zerbal, Debora Passos, Heloisa Hackenhaar, Djaniza Vasques e Bia Zidioti
 
 function animalsByIds(...ids) {
   return animals.filter((animal) => ids.includes(animal.id));
@@ -21,7 +21,7 @@ function animalsByIds(...ids) {
 
 function animalsOlderThan(animal, age) {
   return animals.find((elAnimal) => elAnimal.name === animal)
-    .residents.every((res) => (res.age >= age));
+    .residents.every((resident) => (resident.age >= age));
 }
 
 // function employeeByName(employeeName) {
