061e2fca2be7adbfa20aed45a55ac4025479245f
Willian Mendes <willianmendesbr@gmail.com>
2021-04-08 15:58:54 -0300

    Update (temporary) fix lint

--- src/zoo.js
+++ src/zoo.js
@@ -1,14 +1,3 @@
-/*
-eslint no-unused-vars: [
-  "error",
-  {
-    "args": "none",
-    "vars": "local",
-    "varsIgnorePattern": "data"
-  }
-]
-*/
-
 const data = require('./data');
 
 function animalsByIds(...ids) {
@@ -20,51 +9,51 @@ function animalsByIds(...ids) {
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  return `${animal} ${age}`;
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  return `${employeeName}`;
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  return `${personalInfo} ${associatedWith}`;
 }
 
 function isManager(id) {
-  // seu código aqui
+  return `${id}`;
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+  return `${id} ${firstName} ${lastName} ${managers} ${responsibleFor}`;
 }
 
 function animalCount(species) {
-  // seu código aqui
+  return `${species}`;
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
+  return `${entrants}`;
 }
 
 function animalMap(options) {
-  // seu código aqui
+  return `${options}`;
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  return `${dayName}`;
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
+  return `${id}`;
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  return `${percentage}`;
 }
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
+  return `${idOrName}`;
 }
 
 module.exports = {
