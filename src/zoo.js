3f9bffb35cfc74765af2a5bf60b83d00c7b6c87b
Lucas André <lucas.cancado89@gmail.com>
2021-04-08 15:42:37 -0300

    Requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -35,9 +35,11 @@ function createEmployee(personalInfo, associatedWith) {
   return Object.assign(personalInfo, associatedWith);
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  const allManagers = data.employees.map((employee) => employee.managers);
+  return allManagers.map((aManager) =>
+    aManager.some((manager) => manager === id)).some((item) => item === true);
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -80,7 +82,7 @@ module.exports = {
   employeeByName,
   //   employeeCoverage,
   //   addEmployee,
-  //   isManager,
+  isManager,
   animalsOlderThan,
   //   oldestFromFirstSpecies,
   //   increasePrices,
