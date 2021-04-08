1bf6d8462a7871b4d5e96f62712c78e8c3f02e04
Nathalia Zebral <natzebral@gmail.com>
2021-04-08 19:45:05 -0300

    requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -34,9 +34,10 @@ function employeeByName(employeeName) {
 // seu código aqui
 // }
 
-// function isManager(id) {
-// seu código aqui
-// }
+function isManager(id) {
+  return employees.some((employe) => employe.managers
+    .some((item) => item === id) === true);
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 // seu código aqui
@@ -79,7 +80,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
