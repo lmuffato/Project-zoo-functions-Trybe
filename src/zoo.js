94a9d5f0d50eda4d9b682a33fe39de3cffa96dd0
Patrick Morais <ppternunes@gmail.com>
2021-04-08 14:46:31 -0300

    requisito 6

--- src/zoo.js
+++ src/zoo.js
@@ -40,7 +40,13 @@ function isManager(id) {
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+  employees.push({
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  });
 }
 
 function animalCount(species) {
