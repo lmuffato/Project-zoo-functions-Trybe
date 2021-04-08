952070837c2092037258424e1bd8bc418c4c5ef8
guilhermemd <guilherme.md@gmail.com>
2021-04-08 17:40:34 -0300

    requisito 01

--- src/zoo.js
+++ src/zoo.js
@@ -16,9 +16,9 @@ function animalsByIds(...ids) {
   if (!ids) {
     return [];
   }
-  return animals.filter((animal) => animal.id.includes(ids));
+  return ids.map((id) => animals.find((animal) => animal.id === id));
 }
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+console.log(animalsByIds());
 
 // function animalsOlderThan(animal, age) {
 //   // seu código aqui
