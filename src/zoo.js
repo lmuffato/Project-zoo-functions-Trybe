65ec7765c42f38cf18032caa9b13f63e0d6ee2a7
luanlsr <luanfswd@gmail.com>
2021-04-08 19:40:13 -0300

    Requisito 5 finalizado

--- src/zoo.js
+++ src/zoo.js
@@ -41,9 +41,11 @@ function createEmployee(personalInfo, associatedWith) {
 }
 
 function isManager(id) {
-  // seu código aqui
+  const maneger = employees.map((arr) => arr.managers);
+  return maneger.some((arr, index) => arr[index] === id);
 }
 
+console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
