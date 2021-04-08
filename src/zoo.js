67808c13b94af43c847cf03ef85918bad0f02f77
Debora Passos <deborinhapassos@gmail.com>
2021-04-08 16:22:15 -0300

    Requisito 1 - IMPLEMENTE A FUNÇÃO animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -9,10 +9,11 @@ eslint no-unused-vars: [
 ]
 */
 
+const { animals } = require('./data');
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  return animals.filter((animal) => ids.includes(animal.id));
 }
 
 function animalsOlderThan(animal, age) {
