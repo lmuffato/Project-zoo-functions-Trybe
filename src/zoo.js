6a8c00cd0e2615e4012951f599acd0fc9a43a8fe
lucas-chave <lcchavesr150@gmail.com>
2021-04-09 19:08:05 -0300

    requisito 3

--- src/zoo.js
+++ src/zoo.js
@@ -9,36 +9,39 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');
+const data = require("./data");
 
-function animalsByIds(...ids) {
-  // if (ids.length === 0) return [];
-  return data.animals.filter((specie) => ids.some((id) => id === specie.id) === true);
-}
-
-function animalsOlderThan(animal, age) {
-  // seu código aqui
-  return data.animals.find((element) => element.name === animal)
-    .residents.every((animals) => animals.age >= age);
-}
-
-// function employeeByName(employeeName) {
-//   // seu código aqui
-//   if (employeeByName === undefined) return ;
-//   return data.employees.filter((employee) => employeeName === employee.firstName ||
-//       employee.lastName === employeeName);
+// function animalsByIds(...ids) {
+//   // seu código aqui - ta ok
+//   return data.animals.filter((specie) => ids.some((id) => id === specie.id) === true);
 // }
 
+// function animalsOlderThan(animal, age) {
+//   // seu código aqui - ok
+//   return data.animals.find((element) => element.name === animal)
+//     .residents.every((animals) => animals.age >= age);
+// }
+
+function employeeByName(employeeName) {
+  if (employeeName === undefined) return {};
+
+  if (employeeName) {
+    return data.employees.find((employee) => employeeName === employee
+      .firstName || employeeName === employee
+      .lastName);
+  }
+}
+
+console.log(employeeByName('Wishart'));
 // function createEmployee(personalInfo, associatedWith) {
 //   // seu código aqui
 // }
 
-function isManager(id) {
-  // seu código aqui
-  return data.employees.some((employee) => employee.managers.includes(id));
-}
-// console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83')); // true
-// console.log(isManager('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1')); // false
+// function isManager(id) {
+//   // seu código aqui - ta ok
+//   return data.employees.some((employee) => employee.managers.includes(id));
+// }
+
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
 // }
@@ -72,16 +75,16 @@ function isManager(id) {
 // }
 
 module.exports = {
-//  entryCalculator,
-//  schedule,
-//  animalCount,
-//  animalMap,
-  animalsByIds,
-  // employeeByName,
+  //  entryCalculator,
+  //  schedule,
+  //  animalCount,
+  //  animalMap,
+  //  animalsByIds,
+  employeeByName,
   //   employeeCoverage,
   //   addEmployee,
-  isManager,
-  animalsOlderThan,
+  //  isManager,
+  //  animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
   // createEmployee,
