d015b62b61529082f6f5474ed20ab941b8b93c5a
michael-petterson-06 <michaelpetterson06l@gmailo.com>
2021-04-09 12:43:13 -0300

    createEmployee

--- src/zoo.js
+++ src/zoo.js
@@ -34,11 +34,30 @@ function employeeByName(employeeName) {
   return resposta;
 }
 
-console.log(employeeByName('Wishart'));
+// console.log(employeeByName('Wishart'));
 
-// function createEmployee(personalInfo, associatedWith) {
-//   // seu código aqui
-// }
+function createEmployee(personalInfo, associatedWith) {
+  // 4- seu código aqui
+  return { ...personalInfo, ...associatedWith };
+}
+
+const personalInfo = {
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+};
+
+const associatedWith = {
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'
+  ],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+  ]
+};
 
 // function isManager(id) {
 //   // seu código aqui
@@ -89,5 +108,5 @@ module.exports = {
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
-  // createEmployee,
+  createEmployee,
 };
