ff34245e4eb865193bb36581116cc5755e7e88db
LucasMLara <lucasml@live.com>
2021-04-09 16:16:15 -0300

    refatorando createEmplyee

--- src/zoo.js
+++ src/zoo.js
@@ -11,92 +11,87 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(...ids) {
-  return data.animals.filter(({ id }) => ids.includes(id));
-}
+// function animalsByIds(...ids) {
+//   return data.animals.filter(({ id }) => ids.includes(id));
+// }
 
-animalsByIds();
+// animalsByIds();
 
-function animalsOlderThan(animal, animalAge) {
-  const especie = data.animals.find(({ name }) => name === animal);
-  return especie.residents.every(({ age }) => age > animalAge);
-}
-console.log(animalsOlderThan('penguins', 10));
+// function animalsOlderThan(animal, animalAge) {
+//   const especie = data.animals.find(({ name }) => name === animal);
+//   return especie.residents.every(({ age }) => age > animalAge);
+// }
+// console.log(animalsOlderThan('otters', 8));
 
-function employeeByName(employeeName) {
-  if (!employeeName) return {};
-  return data.employees.find(({ firstName, lastName }) => firstName === employeeName
-  || lastName === employeeName);
-}
+// function employeeByName(employeeName) {
+//   if (!employeeName) return {};
+//   return data.employees.find(({ firstName, lastName }) => firstName === employeeName
+//   || lastName === employeeName);
+// }
+// console.log(employeeByName('Burl'));
 
 function createEmployee(personalInfo, associatedWith) {
-  const { id, firstName, lastName } = personalInfo;
-  const { managers, responsibleFor } = associatedWith;
-  const employee = {
-    id,
-    firstName,
-    lastName,
-    managers,
-    responsibleFor,
-  };
+  // const { id, firstName, lastName } = personalInfo;
+  // const { managers, responsibleFor } = associatedWith;
+  const employee = { ...personalInfo, ...associatedWith };
   return employee;
 }
 
-function isManager(id) {
-  return data.employees.some(({ managers }) => managers.includes(id));
-}
+// function isManager(id) {
+//   return data.employees.some(({ managers }) => managers.includes(id));
+// }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  const employee = {
-    id,
-    firstName,
-    lastName,
-    managers,
-    responsibleFor,
-  };
-  data.employees.push(employee);
-}
+// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//   const employee = {
+//     id,
+//     firstName,
+//     lastName,
+//     managers,
+//     responsibleFor,
+//   };
+//   data.employees.push(employee);
+// }
 
 // function animalCount(species) {
 //   // seu código aqui
 // }
 
-// function entryCalculator(entrants) {
-//   // seu código aqui
-// }
+// // function entryCalculator(entrants) {
+// //   // seu código aqui
+// // }
 
-// function animalMap(options) {
-//   // seu código aqui
-// }
+// // function animalMap(options) {
+// //   // seu código aqui
+// // }
 
-// function schedule(dayName) {
-//   // seu código aqui
-// }
+// // function schedule(dayName) {
+// //   // seu código aqui
+// // }
 
-// function oldestFromFirstSpecies(id) {
-//   // seu código aqui
-// }
+// // function oldestFromFirstSpecies(id) {
+// //   // seu código aqui
+// // }
 
-// function increasePrices(percentage) {
-//   // seu código aqui
-// }
+// // function increasePrices(percentage) {
+// //   // seu código aqui
+// // }
 
-// function employeeCoverage(idOrName) {
-//   // seu código aqui
-// }
+// // function employeeCoverage(idOrName) {
+// //   // seu código aqui
+// // }
 
 module.exports = {
-  // entryCalculator,
-  // schedule,
-  // animalCount,
-  // animalMap,
-  animalsByIds,
-  employeeByName,
-  // employeeCoverage,
-  addEmployee,
-  isManager,
-  animalsOlderThan,
-  // oldestFromFirstSpecies,
-  // increasePrices,
+//   // entryCalculator,
+//   // schedule,
+//   animalCount,
+//   // animalMap,
+//   animalsByIds,
+  // employeeByName,
+//   // employeeCoverage,
+//   addEmployee,
+//   isManager,
+  // animalsOlderThan,
+//   // oldestFromFirstSpecies,
+//   // increasePrices,
   createEmployee,
 };
