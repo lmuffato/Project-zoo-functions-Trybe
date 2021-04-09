0fc171a92363cb3cdac50444941f9b689fc6ecc7
Lucas Pedroso <lepsk8@gmail.com>
2021-04-09 12:23:44 -0300

    feat(animalMap): implemented.

--- src/zoo.js
+++ src/zoo.js
@@ -59,19 +59,68 @@ function animalCount(species) {
   return data.animals.find(({ name }) => name === species).residents.length;
 }
 
+const sumPrice = (acc, [person, qty]) => acc + data.prices[person] * qty;
 function entryCalculator(entrants = 0) {
-  return Object.entries(entrants).reduce((acc, [person, qty]) => {
-    return acc + data.prices[person] * qty;
-  }, 0);
+  return Object.entries(entrants).reduce(sumPrice, 0);
 }
 
+const localeAnimals = (acc, curr) => {
+  if (acc[curr.location]) {
+    acc[curr.location].push(curr.name);
+  } else {
+    acc[curr.location] = [curr.name];
+  }
+  return acc;
+};
+const getAnimalsPerLocation = () => data.animals.reduce(localeAnimals, {});
+
+const getAllResidentsNames = (isSorted, sexAnimal = 'all') => (acc, { name, sex }) => {
+  if (sexAnimal === 'all' || sexAnimal === sex) acc.push(name);
+  if (isSorted) acc.sort();
+  return acc;
+};
+const createOrAddResident = (isSorted, sexAnimal) => (acc, { name, location, residents }) => {
+  if (acc[location]) {
+    acc[location].push({ [name]: residents.reduce(getAllResidentsNames(isSorted, sexAnimal), []) });
+  } else {
+    acc[location] = [{ [name]: residents.reduce(getAllResidentsNames(isSorted, sexAnimal), []) }];
+  }
+  return acc;
+};
+const getAnimalsPerLocationIncludesTrue = (isSorted, sexAnimal) =>
+  data.animals.reduce(createOrAddResident(isSorted, sexAnimal), {});
+
+const getAnimalsPerLocationWithIncludes = (hasIncludeNames, isSorted, sexAnimal) => {
+  if (hasIncludeNames) {
+    if (sexAnimal !== undefined) return getAnimalsPerLocationIncludesTrue(isSorted, sexAnimal);
+    if (isSorted && sexAnimal === undefined) return getAnimalsPerLocationIncludesTrue(isSorted);
+    return getAnimalsPerLocationIncludesTrue();
+  }
+  return getAnimalsPerLocation();
+};
 
 function animalMap(options) {
-  // seu código aqui
+  let output = getAnimalsPerLocation();
+  if (!options) return output;
+  const hasIncludeNames = !!options.includeNames;
+  const isSorted = !!options.sorted;
+  const sexAnimal = options.sex;
+
+  output = getAnimalsPerLocationWithIncludes(hasIncludeNames, isSorted, sexAnimal);
+  return output;
 }
 
+const getWeek = (dayName) => (acc, [key, value]) => {
+  if (dayName === undefined || dayName === key) {
+    acc[key] = value.open !== 0
+      ? `Open from ${value.open}am until ${value.close - 12}pm`
+      : 'CLOSED';
+  }
+  return acc;
+};
 function schedule(dayName) {
-  // seu código aqui
+  // (!dayName) return
+  return Object.entries(data.hours).reduce(getWeek(dayName), {});
 }
 
 function oldestFromFirstSpecies(id) {
