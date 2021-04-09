40081592c66012de344fb5433e7421b839a6f248
lucas-chave <lcchavesr150@gmail.com>
2021-04-09 20:01:56 -0300

    requisito 4

--- src/zoo.js
+++ src/zoo.js
@@ -23,6 +23,8 @@ function animalsOlderThan(animal, age) {
 }
 
 function employeeByName(employeeName) {
+  // seu código aqui - ok
+
   if (employeeName === undefined) return {};
   if (employeeName) {
     return data.employees.find((employee) => employeeName === employee
@@ -31,9 +33,35 @@ function employeeByName(employeeName) {
   }
 }
 
-// function createEmployee(personalInfo, associatedWith) {
-//   // seu código aqui
-// }
+const obj1 = {
+  id: '7ed1c9bb-8570-44f6-b718-0666b869573a',
+  firstName: 'John',
+  lastName: 'Doe',
+};
+const obj2 = {
+  managers: [
+    'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+    '9e7d4524-363c-416a-8759-8aa7e50c0992'
+  ],
+  responsibleFor: [
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+    'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'
+  ]
+};
+function createEmployee(personalInfo, associatedWith) {
+  // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+}
+console.log(createEmployee(obj1, obj2));
 
 function isManager(id) {
   // seu código aqui - ta ok
@@ -80,10 +108,10 @@ module.exports = {
   animalsByIds,
   employeeByName,
   //   employeeCoverage,
-  //   addEmployee,
+  //  addEmployee,
   isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
-  // createEmployee,
+  createEmployee,
 };
