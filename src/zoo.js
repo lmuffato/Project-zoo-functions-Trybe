253f014935cfc84ddeb99f9ac7c7dc6cd4f9bbd3
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-04-09 11:58:52 -0300

    Requisito 4 -Criaçao do Empregado

--- src/zoo.js
+++ src/zoo.js
@@ -26,11 +26,11 @@ function employeeByName(employeeName) {
   return employees.find((empregado) =>
     empregado.firstName === employeeName || empregado.lastName === employeeName);
 }
-/*
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
 
+function createEmployee(personalInfo, associatedWith) {
+  return { ...personalInfo, ...associatedWith };
+}
+/*
 function isManager(id) {
   // seu código aqui
 }
@@ -80,5 +80,5 @@ module.exports = {
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
-  // createEmployee,
+  createEmployee,
 };
