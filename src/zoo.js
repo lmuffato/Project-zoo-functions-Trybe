70da9b424d96b71d95d8d88a856d075ab48d16d1
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-04-09 08:55:02 -0300

    enviando o requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -46,9 +46,9 @@ function createEmployee(personalInfo, associatedWith) {
   };
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  return (data.employees.find((employee) => employee.managers.includes(id)) !== undefined);
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -91,7 +91,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
