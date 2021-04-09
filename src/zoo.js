25a7af65830213cf563d7db8bf602f6dc6772e4b
Southier <patrick.southier@hotmail.com>
2021-04-09 18:03:22 -0300

    Completes requirement 5.

--- src/zoo.js
+++ src/zoo.js
@@ -43,9 +43,9 @@ function createEmployee(personalInfo, associatedWith) {
   return newEmployeeArray;
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  return employees.some((employee, index) => employee.managers[index] === id);
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -88,7 +88,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
