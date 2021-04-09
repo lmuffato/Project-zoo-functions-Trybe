a6a0d5674c4eda729a540c3838bda1e50de0d81a
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-04-09 15:21:33 -0300

    requirement 'IMPLEMENTE A FUNÇÃO isManager' done

--- src/zoo.js
+++ src/zoo.js
@@ -42,9 +42,12 @@ function createEmployee(personalInfo, associatedWith) {
   return newEmployee;
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  const { employees } = data;
+  const findManager = employees.some(({ managers }) => managers.find((manager) => manager === id));
+
+  return findManager;
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -87,7 +90,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
