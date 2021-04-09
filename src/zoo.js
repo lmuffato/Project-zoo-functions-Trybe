3891c4e13733ecff3181da5f5fdda08166dcf675
Max Rudim <max.hermsdorff@gmail.com>
2021-04-09 17:25:11 -0300

    Reqs: 07 - AnimalCount e 08 - entryCalculator

--- src/zoo.js
+++ src/zoo.js
@@ -8,7 +8,7 @@ eslint no-unused-vars: [
   }
 ]
 */
-const { animals, employees } = require('./data');
+const { animals, employees, prices } = require('./data');
 // const data = require('./data');
 
 function animalsByIds(...ids) {
@@ -63,13 +63,29 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
   employees.push(employee);
 }
 
-// function animalCount(species) {
+function animalCount(species) {
 // seu código aqui
-// }
+// Referência: nome de usuário GitHub: LucasCAndre (https://github.com/tryber/sd-010-a-project-zoo-functions/pull/39/files)
+  if (species === undefined) {
+    const obj = {}; // object em que será retornado os elementos caso o parâmetro species esteja vazio
+    animals.forEach((element) => { // iteração passando por cada animal(element) no array animals
+      obj[element.name] = element.residents.length; // adiciona ao objeto uma chave referente a cada animal encontrado
+    }); // às chaves acima, é atribuído value igual à quantidade de residentes
+    return obj; // retorna o objeto contendo nome do animal + quantidade de residentes
+  }
+  return animals.find((animal) => animal.name === species).residents.length; // compara o parâmetro com os resultados do find e retorna apenas a quantidade de elementos em residents
+}
 
-// function entryCalculator(entrants) {
+function entryCalculator(entrants) {
 // seu código aqui
-// }
+  if (entrants === undefined) return 0;
+  const { Adult = 0, Senior = 0, Child = 0 } = entrants;
+  const totalAdultPrices = Adult * prices.Adult;
+  const totalSeniorPrices = Senior * prices.Senior;
+  const totalChildPrices = Child * prices.Child;
+  const totalValue = totalAdultPrices + totalSeniorPrices + totalChildPrices;
+  return totalValue;
+}
 
 // function animalMap(options) {
 // seu código aqui
@@ -92,9 +108,9 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 // }
 
 module.exports = {
-  // entryCalculator,
+  entryCalculator,
   // schedule,
-  // animalCount,
+  animalCount,
   // animalMap,
   animalsByIds,
   employeeByName,
