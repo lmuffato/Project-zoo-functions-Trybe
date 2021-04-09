0d1a88dba22bdc0462b03a1a2490ac136ffc2d45
Jodiel Briesemeister <jodielll29@gmail.com>
2021-04-08 21:22:30 -0300

    Requisitos 1-7

--- src/zoo.js
+++ src/zoo.js
@@ -9,39 +9,59 @@ eslint no-unused-vars: [
 ]
 */
 
-const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+const data = require('./data');
+const { animals, employees } = data;
+
+function animalsByIds(...ids) {
+  const animalsArray = [];
+  ids.forEach((idParam, index) => (animals[index].id === idParam ? animalsArray.push(animals[index]):animalsArray))
+  return animalsArray;
 }
 
-function animalsOlderThan(animal, age) {
-  // seu código aqui
+function animalsOlderThan(name, age) {
+  const objAnimal = animals.find((animal) => animal.name === name);
+  return result = objAnimal.residents.every((animal) => animal.age >= age ? true:false);
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  return employees.find((emp) => (emp.firstName === employeeName || emp.lastName === employeeName))
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const {id, firstName, lastName} = personalInfo;
+  const {managers, responsibleFor} = associatedWith;
+
+  const obj = {id,firstName,lastName,managers,responsibleFor};
+  return obj;
 }
 
 function isManager(id) {
-  // seu código aqui
+  const managerList = [];
+  employees.forEach((emp) => (emp.managers.length > 0 ? managerList.push(...emp.managers):emp.managers))
+  const check = managerList.find((gerenteId) => gerenteId === id)
+  return check ? true:false;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  return employees.push({id, firstName, lastName, managers, responsibleFor})
 }
 
 function animalCount(species) {
-  // seu código aqui
+  const listAnimals = {};
+  data.animals.forEach((animal) => Object.assign(listAnimals, {[animal.name]:animal.residents.length}));
+  if (!species) {return listAnimals}
+  return listAnimals[species]
 }
 
 function entryCalculator(entrants) {
   // seu código aqui
+  if (!entrants || entrants.length === 0 || entrants.length === undefined) {return 0} 
+  return 'test'
+  
 }
+console.log(entryCalculator({}));
 
 function animalMap(options) {
   // seu código aqui
