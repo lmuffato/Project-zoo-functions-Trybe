e02f099e1f2c2480de654be269b4432513349ef3
Paulo H. Lopes <phlopes93@gmail.com>
2021-04-09 15:38:06 -0300

    requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -41,9 +41,9 @@ function createEmployee(personalInfo, associatedWith) {
   };
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  return employees.some(({ managers }, i) => managers[i] === id);
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -86,7 +86,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
