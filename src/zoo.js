37f09136eaf22dcb4cdef5bbb3ad07f86fe743c1
root <root@DESKTOP-74CRLKE.localdomain>
2021-04-10 18:03:27 -0300

    requisito 1

--- src/zoo.js
+++ src/zoo.js
@@ -9,11 +9,11 @@ eslint no-unused-vars: [
 ]
 */
 
-const { animals } = require('./data');
 const data = require('./data');
 
 function animalsByIds(...ids) {
   // seu código aqui
+  const { animals } = data;
   return animals.filter(({ element }) => ids.includes(element));
 }
 
