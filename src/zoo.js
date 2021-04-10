e32c83211b77595e19647a6782462ea98be4edca
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-04-10 13:28:23 -0300

    Desafio 1 OK

--- src/zoo.js
+++ src/zoo.js
@@ -9,9 +9,8 @@ eslint no-unused-vars: [
 ]
 */
 
-const animals = require('./data');
-const employees = require('./data');
-// const data = require('./data');
+const { animals, employees } = require('./data');
+const data = require('./data');
 
 function animalsByIds(...ids) {
   if (ids.length === 0) return [];
@@ -49,8 +48,6 @@ function employeeByName(employeeName) {
     firstName === employeeName || lastName === employeeName);
 }
 
-// console.log(employeeByName());
-
 function createEmployee(personalInfo, associatedWith) {
   const { id, firstName, lastName } = personalInfo;
   const { managers, responsibleFor } = associatedWith;
@@ -66,8 +63,6 @@ function isManager(id) {
   return employees.some((emp) => emp.managers.includes(id));
 }
 
-// console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
-
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   const newEmployee = {
     id,
@@ -77,7 +72,6 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
     responsibleFor,
   };
   employees.push(newEmployee);
-  // return console.log(employees);
 }
 
 function animalCount(species) {
