0106e7fd2a88e91e357b9f4cbf3893c93e76946d
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-04-11 18:16:54 -0300

    Enviando o requisito 10

--- src/zoo.js
+++ src/zoo.js
@@ -8,17 +8,6 @@ eslint no-unused-vars: [
   }
 ]
 */
-// const lionId = '0938aa23-f153-4937-9f88-4858b24d6bce';
-// const ottersId = '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae';
-// const elephantsId = 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5';
-// const snakesId = '78460a91-f4da-4dea-a469-86fd2b8ccc84';
-// const frogsId = '89be95b3-47e4-4c5b-b687-1fabf2afa274';
-// const bearsId = 'baa6e93a-f295-44e7-8f70-2bcdc6f6948d';
-// const tigersId = 'e8481c1d-42ea-4610-8e11-1752cfc05a46';
-// /* ============================================================ */
-// const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
-// const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
-// const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
 
 const data = require('./data');
 
@@ -27,10 +16,13 @@ function animalsByIds(...ids) {
   return ids.map((id) => data.animals.find((animal) => animal.id === id));
 }
 
+// Encontra o objeto do animal com o nome animalName
+// Acessa a propriedade residents e cria um array com as idades
+// Por fim verifica se cada uma dessas idades são maiores que age
 function animalsOlderThan(animalName, age) {
-  return data.animals.find((animal) =>// Encontra o objeto do animal com o nome animalName
-    animal.name === animalName).residents.map((resident) =>// Acessa a propriedade residents e cria um array com as idades
-    resident.age).every((howOld) => howOld > age);// Por fim verifica se cada uma dessas idades são maiores que age
+  return data.animals.find((animal) =>
+    animal.name === animalName).residents.map((resident) =>
+    resident.age).every((howOld) => howOld > age);
 }
 
 // Pega todos os objetos dentro de data.employees, obtem os valores das chaves e verifica se está incluído employeeName
@@ -95,14 +87,14 @@ function entryCalculator(entrants) {
 //         animal.location === curr).map((located) => located.name);
 //       return acc;
 //     }, {});
-//   } 
-  
+//   }
+
 //   else {
 //     return locations.reduce((acc, curr) => {
 //       acc[curr] = data.animals.filter((animal) =>
 //         animal.location === curr).map((located) => {
 //         const obj = {};
-//         (options.sorted === true) ? 
+//         (options.sorted === true) ?
 //         obj[located.name] = located.residents.reduce((arrayResidents, currentResident) => {
 //           if (options.sex === undefined) {
 //             arrayResidents.push(currentResident.name);
@@ -115,7 +107,7 @@ function entryCalculator(entrants) {
 //           if (options.sex === undefined) {
 //             arrayResidents.push(currentResident.name);
 //           }
-          
+
 //           else if (currentResident.sex === 'female') {
 //           arrayResidents.push(currentResident.name);
 //           }
@@ -128,9 +120,16 @@ function entryCalculator(entrants) {
 //   }
 // }
 
-// function schedule(dayName) {
-//   // seu código aqui
-// }
+function schedule(dayN) {
+  let days = Object.keys(data.hours);
+  if (dayN !== undefined) { days = [dayN]; }
+  return days.reduce((daysObj, day) => {
+    Object.assign(daysObj,
+      { [day]: `Open from ${data.hours[day].open}am until ${data.hours[day].close - 12}pm` });
+    if (day === 'Monday') { Object.assign(daysObj, { [day]: 'CLOSED' }); }
+    return daysObj;
+  }, {});
+}
 
 // function oldestFromFirstSpecies(id) {
 //   // seu código aqui
@@ -146,7 +145,7 @@ function entryCalculator(entrants) {
 
 module.exports = {
   entryCalculator,
-  // schedule,
+  schedule,
   animalCount,
   // animalMap,
   animalsByIds,
