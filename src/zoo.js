fcd3be20990e27e6d363b2f600cbced6b8c47b16
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-04-11 20:21:07 -0300

    requisito 6 usando default values na funcao

--- src/zoo.js
+++ src/zoo.js
@@ -39,8 +39,9 @@ function isManager(id) {
   return result;
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const newEmployee = { id, firstName, lastName, managers, responsibleFor };
+  data.employees.push(newEmployee);
 }
 
 function animalCount(species) {
