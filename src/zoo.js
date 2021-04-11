8b6a4d436b792b7a97dd4a55cccd9980358e2f2f
William Willers <w.willers@outlook.com>
2021-04-11 18:56:47 -0400

    Quinto requisito

--- src/zoo.js
+++ src/zoo.js
@@ -38,10 +38,9 @@ function createEmployee(personalInfo, associatedWith) {
   };
   return newEmployee;
 }
-// function isManager(id) {
-//   // seu código aqui
-// }
-
+function isManager(id) {
+  return data.employees.some((managerName) => managerName.managers.includes(id));
+}
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
 // }
@@ -83,7 +82,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
