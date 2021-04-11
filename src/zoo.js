99a7e732f8daa87d81ccd3b845000681172c3190
lucas-chave <lcchavesr150@gmail.com>
2021-04-11 16:20:02 -0300

    entryClaculator

--- src/zoo.js
+++ src/zoo.js
@@ -11,72 +11,71 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-// function animalsByIds(...ids) {
-//   // seu código aqui - ta ok
-//   return data.animals.filter((specie) => ids.some((id) => id === specie.id) === true);
-// }
+function animalsByIds(...ids) {
+  // seu código aqui - ta ok
+  return data.animals.filter((specie) => ids.some((id) => id === specie.id) === true);
+}
 
-// function animalsOlderThan(animal, age) {
-//   // seu código aqui - ok
-//   return data.animals.find((element) => element.name === animal)
-//     .residents.every((animals) => animals.age >= age);
-// }
+function animalsOlderThan(animal, age) {
+  // seu código aqui - ok
+  return data.animals.find((element) => element.name === animal)
+    .residents.every((animals) => animals.age >= age);
+}
 
-// function employeeByName(employeeName) {
-//   // seu código aqui - ok
+function employeeByName(employeeName) {
+  // seu código aqui - ok
 
-//   if (employeeName === undefined) return {};
-//   if (employeeName) {
-//     return data.employees.find((employee) => employeeName === employee
-//       .firstName || employeeName === employee
-//       .lastName);
-//   }
-// }
+  if (employeeName === undefined) return {};
+  if (employeeName) {
+    return data.employees.find((employee) => employeeName === employee
+      .firstName || employeeName === employee
+      .lastName);
+  }
+}
 
-// function createEmployee(personalInfo, associatedWith) {
-//   // seu código aqui
-//   const { id, firstName, lastName } = personalInfo;
-//   const { managers, responsibleFor } = associatedWith;
-//   return {
-//     id,
-//     firstName,
-//     lastName,
-//     managers,
-//     responsibleFor,
-//   };
-// }
+function createEmployee(personalInfo, associatedWith) {
+  // seu código aqui
+  const { id, firstName, lastName } = personalInfo;
+  const { managers, responsibleFor } = associatedWith;
+  return {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+}
 
-// function isManager(id) {
-//   // seu código aqui - ta ok
-//   return data.employees.some((employee) => employee.managers.includes(id));
-// }
+function isManager(id) {
+  // seu código aqui - ta ok
+  return data.employees.some((employee) => employee.managers.includes(id));
+}
 
-// function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
-//   // seu código aqui - ta ok
-//   const employeeNew = {
-//     id,
-//     firstName,
-//     lastName,
-//     managers,
-//     responsibleFor,
-//   };
-//   data.employees.push(employeeNew);
-// }
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  // seu código aqui - ta ok
+  const employeeNew = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+  data.employees.push(employeeNew);
+}
 
-// function animalCount(species) {
-//   // seu código aqui
-//   if (species === undefined) {
-//     return data.animals.reduce((acc, curr) => {
-//       acc[curr.name] = curr.residents.length;
-//       return acc;
-//     }, {});
-//   }
+function animalCount(species) {
+  // seu código aqui
+  if (species === undefined) {
+    return data.animals.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
 
-//   if (species !== undefined) {
-//     return data.animals.find((animal) => animal.name === species).residents.length;
-//   }
-// }
-// console.log(animalCount());
+  if (species !== undefined) {
+    return data.animals.find((animal) => animal.name === species).residents.length;
+  }
+}
 
 function entryCalculator(entrants) {
   if (!entrants || Object.keys(entrants).length === 0) return 0;
@@ -84,7 +83,6 @@ function entryCalculator(entrants) {
   const { Adult: Bigger = 0, Senior: OldMan = 0, Child: Kid = 0 } = entrants;
   return Adult * Bigger + Senior * OldMan + Child * Kid;
 }
-console.log(entryCalculator({ 'Senior': 3 }));
 // function animalMap(options) {
 //   // seu código aqui
 // }
@@ -118,15 +116,15 @@ console.log(entryCalculator({ 'Senior': 3 }));
 module.exports = {
   entryCalculator,
   //  schedule,
-  // animalCount,
+  animalCount,
   //  animalMap,
-  // animalsByIds,
-  // employeeByName,
+  animalsByIds,
+  employeeByName,
   //   employeeCoverage,
-  // addEmployee,
-  // isManager,
-  // animalsOlderThan,
+  addEmployee,
+  isManager,
+  animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
-  // createEmployee,
+  createEmployee,
 };
