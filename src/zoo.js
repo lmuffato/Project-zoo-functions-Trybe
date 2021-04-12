d0a901e9b0755a0056d992b382cddeb8e6e374e7
renancosta21 <renancosta21@gmail.com>
2021-04-12 15:27:10 -0300

    iniciando os trabalhos

--- src/zoo.js
+++ src/zoo.js
@@ -12,7 +12,7 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(ids) {
-  // seu código aqui
+  // seu código aqui - iniciando os trabalhos
 }
 
 function animalsOlderThan(animal, age) {
