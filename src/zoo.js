e0abe2b0f1ab41555c9f176866aa4e44f923c602
Lucas Muffato <lmiremuffato@gmail.com>
2021-04-12 10:30:59 -0300

    requisito 01

--- src/zoo.js
+++ src/zoo.js
@@ -12,9 +12,9 @@ eslint no-unused-vars: [
 const { animals } = require('./data');
 // const data = require('./data');
 
-function animalsByIds (...ids) {
-  return animals.filter((animais) => ids.includes(animais.id))
-};
+function animalsByIds(...ids) {
+  return animals.filter((animais) => ids.includes(animais.id));
+}
 
 // console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '89be95b3-47e4-4c5b-b687-1fabf2afa274'));// test 1
 // console.log(animalsByIds());// test 2
