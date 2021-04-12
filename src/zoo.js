9c962e2c647ff52725063d2cd88d7f083914e5fb
camiloLelis <lafirmafuerte@gmail.com>
2021-04-12 11:52:38 -0300

    requisito3-tentativa4

--- src/zoo.js
+++ src/zoo.js
@@ -32,8 +32,8 @@ function animalsOlderThan(animal, age) {
 function employeeByName(employeeName) {
   // seu código aqui
   if (!employeeName) return {};
-  return data
-    .employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeNa);
+  return data.employees
+    .find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 /* function createEmployee(personalInfo, associatedWith) {
