de6bbb6977b464764c1e16cfcd79832353f85918
andy silva <andssilva.23@gmail.com>
2021-04-12 21:18:55 -0400

    feat(questão 1): :rocket: filtra animais por id

--- src/zoo.js
+++ src/zoo.js
@@ -11,9 +11,12 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+const { animals } = data;
+
+function animalsByIds(...ids) {
+  return animals.filter(({ id }) => ids.includes(id));
 }
+// console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
 function animalsOlderThan(animal, age) {
   // seu código aqui
