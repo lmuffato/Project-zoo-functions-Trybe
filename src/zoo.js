976bf28220226c32e59f1a6240aead4772f44d32
andy silva <andssilva.23@gmail.com>
2021-04-12 21:27:38 -0400

    fix(questão 1): :bug: corrige erros do lint

--- src/zoo.js
+++ src/zoo.js
@@ -18,66 +18,66 @@ function animalsByIds(...ids) {
 }
 // console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 
-function animalsOlderThan(animal, age) {
+/* function animalsOlderThan(animal, age) {
   // seu código aqui
-}
+} */
 
-function employeeByName(employeeName) {
+/* function employeeByName(employeeName) {
   // seu código aqui
-}
+} */
 
-function createEmployee(personalInfo, associatedWith) {
+/* function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
-}
+} */
 
-function isManager(id) {
+/* function isManager(id) {
   // seu código aqui
-}
+} */
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+/* function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
-}
+} */
 
-function animalCount(species) {
+/* function animalCount(species) {
   // seu código aqui
-}
+} */
 
-function entryCalculator(entrants) {
+/* function entryCalculator(entrants) {
   // seu código aqui
-}
+} */
 
-function animalMap(options) {
+/* function animalMap(options) {
   // seu código aqui
-}
+} */
 
-function schedule(dayName) {
+/* function schedule(dayName) {
   // seu código aqui
-}
+} */
 
-function oldestFromFirstSpecies(id) {
+/* function oldestFromFirstSpecies(id) {
   // seu código aqui
-}
+} */
 
-function increasePrices(percentage) {
+/* function increasePrices(percentage) {
   // seu código aqui
-}
+} */
 
-function employeeCoverage(idOrName) {
+/* function employeeCoverage(idOrName) {
   // seu código aqui
-}
+} */
 
 module.exports = {
-  entryCalculator,
+  /* entryCalculator,
   schedule,
   animalCount,
-  animalMap,
+  animalMap, */
   animalsByIds,
-  employeeByName,
+  /* employeeByName,
   employeeCoverage,
   addEmployee,
   isManager,
   animalsOlderThan,
   oldestFromFirstSpecies,
   increasePrices,
-  createEmployee,
+  createEmployee, */
 };
