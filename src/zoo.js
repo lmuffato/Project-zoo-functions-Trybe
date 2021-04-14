612941324b91b09ff601fedb4d57007a72c47551
Lucas Muffato <lmiremuffato@gmail.com>
2021-04-14 13:42:04 -0300

    requisito 07

--- src/zoo.js
+++ src/zoo.js
@@ -70,16 +70,19 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 //             employees) // TESTE 1 - Retorna o objeto employee com o novo empregado no final da lista
 
 function animalCount(specie) {
-  if (!specie) {
-    return animals.map(({ name, residents }) => ({ [name]: residents.length }));
+    if (!specie) {
+    const animalList = {};
+    animals.map(({ name, residents }) => {animalList[name] = residents.length;
+    });
+    return animalList;
   }
   return (animals.find((animal) => animal.name === specie)).residents.length;
 }
-// console.log ( animalCount('lions') ) // TESTE 1 -> Retorna 4, que é a população de leões
-// console.log ( animalCount() ) // TESTE 2 -> Retorna um objeto contendo o nome do animale a população
+console.log ( animalCount('lions') ) // TESTE 1 -> Retorna 4, que é a população de leões
+console.log ( animalCount() ) // TESTE 2 -> Retorna um objeto contendo o nome do animale a população
 
 // function entryCalculator(entrants) {
-//   // seu código aqui
+
 // }
 
 // function animalMap(options) {
@@ -103,7 +106,7 @@ function animalCount(specie) {
 // }
 
 module.exports = {
-  //   entryCalculator,
+  // entryCalculator,
   //   schedule,
   animalCount,
   //   animalMap,
