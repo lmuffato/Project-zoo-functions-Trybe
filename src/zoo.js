fc3df45d279e0a1044589b7b354d5665234dd795
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-04-13 22:48:54 -0300

    Requesito3, Eslint4

--- src/zoo.js
+++ src/zoo.js
@@ -26,7 +26,7 @@ function employeeByName(employeeName) {
   if (!employeeName) return {};
   return employees.find(
     (employee) => employee.firstName === employeeName || employee.lastName === employeeName
-    );
+  )
 }
 
 // function createEmployee(personalInfo, associatedWith) {
