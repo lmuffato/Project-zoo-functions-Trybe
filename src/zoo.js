b15375d6ff44b7b158d8820c9d7fd070095989af
Lucas Muffato <lmiremuffato@gmail.com>
2021-04-14 11:09:19 -0300

    requisito 07

--- src/zoo.js
+++ src/zoo.js
@@ -69,9 +69,15 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 //             ['baa6e93a-f295-44e7-8f70-2bcdc6f6948d', '0938aa23-f153-4937-9f88-4858b24d6bce']),
 //             employees) // TESTE 1 - Retorna o objeto employee com o novo empregado no final da lista
 
-// function animalCount(species) {
-//   // seu código aqui
-// }
+function animalCount(species) {
+  if(!species){
+    return animals.map(({name, residents}) => ({ [name]:residents.length }));
+  }
+  return animals.find(({ name }) => name = species).residents.length;
+  // return animals.reduce((acumulador, { residents }) => acumulador + residents.length, 0) // Animais totais
+}
+// console.log ( animalCount('lions') ) // TESTE 1 -> Retorna 4, que é a população de leões
+// console.log ( animalCount() ) // TESTE 2 -> Retorna um objeto contendo o nome do animale a população
 
 // function entryCalculator(entrants) {
 //   // seu código aqui
@@ -100,7 +106,7 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
 module.exports = {
   //   entryCalculator,
   //   schedule,
-  //   animalCount,
+  animalCount,
   //   animalMap,
   animalsByIds,
   employeeByName,
