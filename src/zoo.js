9f3fb8162ae37ab0e4fa89df0e193186d2ef72ba
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-04-13 22:43:12 -0300

    Requesito3, Eslint

--- src/zoo.js
+++ src/zoo.js
@@ -25,7 +25,7 @@ function animalsOlderThan(animal, age) {
 function employeeByName(employeeName) {
   if (!employeeName) return {};
   return employees.find(
-    employee => employee.firstName === employeeName || employee.lastName === employeeName);
+    (employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 // function createEmployee(personalInfo, associatedWith) {
