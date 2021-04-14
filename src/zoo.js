c68f47f65328e3d7a2457880953f9afd8243cb85
Lucas Muffato <lmiremuffato@gmail.com>
2021-04-14 13:17:57 -0300

    requisito 07

--- src/zoo.js
+++ src/zoo.js
@@ -73,7 +73,7 @@ function animalCount(specie) {
   if (!specie) {
     return animals.map(({ name, residents }) => ({ [name]: residents.length }));
   }
-  return (animals.find((animal) => animal.name = specie)).residents.length;
+  return (animals.find((animal) => animal.name === specie)).residents.length;
 }
 // console.log ( animalCount('lions') ) // TESTE 1 -> Retorna 4, que é a população de leões
 // console.log ( animalCount() ) // TESTE 2 -> Retorna um objeto contendo o nome do animale a população
