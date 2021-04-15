bd5f12be8cfdc1692d66dd7babf61a3854ac27e4
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-04-15 19:18:28 -0300

    inclusao do requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -41,9 +41,10 @@ function createEmployee(personalInfo, associatedWith) {
   return { ...personalInfo, ...associatedWith };
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  const { employees } = data;
+  return employees.some(({ managers }) => managers.includes(id));
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -86,7 +87,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
