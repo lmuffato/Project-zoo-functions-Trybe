047cce9c53956a486532a24ca27f7112b3cdd991
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-04-15 18:44:13 -0300

    lint linha 41

--- src/zoo.js
+++ src/zoo.js
@@ -38,7 +38,7 @@ function employeeByName(employeeName) {
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  return { ...personalInfo, ...associatedWith};
+  return { ...personalInfo, ...associatedWith };
 }
 
 // function isManager(id) {
