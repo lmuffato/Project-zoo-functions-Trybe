8989bf3f32f02940de8f17845bfc20abd93743d7
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-04-15 17:22:50 -0300

    lint

--- src/zoo.js
+++ src/zoo.js
@@ -34,8 +34,8 @@ function employeeByName(employeeName) {
   // const employeeFirst = employees.find((employeeBuscado) =>employeeBuscado.firstName === employeeName);
   // const employeeLast = employees.find((employeeBuscado) =>employeeBuscado.lastName === employeeName);
   if (employeeName === undefined) return {};
-  return employees.find((employeeBuscado) => 
-    employeeBuscado.firstName === employeeName || employeeBuscado.lastName === employeeName);
+  return employees.find((name) => 
+    name.firstName === employeeName || name.lastName === employeeName);
 }
 
 // function createEmployee(personalInfo, associatedWith) {
