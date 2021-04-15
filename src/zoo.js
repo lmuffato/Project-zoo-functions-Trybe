47522ac3f72ade4ae84f1abf0e1a2d76f994508e
JuliaBaptista <juliabaptistadeoliveira@gmail.com>
2021-04-14 21:30:48 -0300

    Conclusao requisito 05

--- src/zoo.js
+++ src/zoo.js
@@ -44,9 +44,10 @@ function createEmployee(personalInfo, associatedWith) {
   };
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  return employees.some((employeeObj) => employeeObj.managers.some((employeeId) =>
+  employeeId === id));
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -89,7 +90,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
