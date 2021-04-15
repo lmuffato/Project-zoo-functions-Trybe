59f47d571a26e75fd6a63861d7e11291049d2565
renancosta21 <renancosta21@gmail.com>
2021-04-15 18:44:06 -0300

    requisito 5

--- src/zoo.js
+++ src/zoo.js
@@ -41,9 +41,15 @@ function createEmployee(personalInfo, associatedWith) {
   return resultObj;
 }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  const managersArray = [];
+  employees.forEach((em) => (
+    em.managers.length > 0 ? managersArray.push(...em.managers) : em.managers
+  ));
+  const check = managersArray.find((managerId) => managerId === id);
+  const bool = check !== undefined;
+  return bool;
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -86,7 +92,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
