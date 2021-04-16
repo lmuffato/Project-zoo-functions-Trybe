2fe1a798b5fc6028e921c023b54be34b0f50a880
camiloLelis <lafirmafuerte@gmail.com>
2021-04-16 13:16:58 -0300

    requisito-11-tentativa-3

--- src/zoo.js
+++ src/zoo.js
@@ -125,8 +125,7 @@ function schedule(dayName) {
   return { dayName: 'CLOSED' };
 }
 
-const animalProcurado = (id) =>{
-  return data
+const animalProcurado = (id) => { return data
     .employees.find((employee) => id === employee.id).responsibleFor
     .find((animal) => animal);
 };
@@ -134,7 +133,7 @@ const animalProcurado = (id) =>{
 function oldestFromFirstSpecies(id) {
   // seu código aqui
   return Object.values(data.animals.find((animal) => animal.id === animalProcurado(id)).residents
-    .reduce((antes, depois) =>(antes.age > depois.age) ? antes:depois));
+    .reduce((antes, depois) => antes.age > depois.age ? antes : depois));
 }
 
 /* function increasePrices(percentage) {
