7bcb93f3442ae607308fe9d7a3414008f84a5421
Renata Andrade <reapminas@gmail.com>
2021-04-18 15:40:14 -0300

    Requisito 5. IMPLEMENTE A FUNÇÃO isManager

--- src/zoo.js
+++ src/zoo.js
@@ -33,9 +33,9 @@ function employeeByName(name) {
 //   // seu código aqui
 // }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  return employees.some((option) => (option.managers.includes(id)));
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -78,7 +78,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   // animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
