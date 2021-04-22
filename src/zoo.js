a1ad60e2ed81321ece7b391d047f337115860c9f
Apoc35 <alexv_silveira@outlook.com>
2021-04-21 23:42:52 -0300

    Resolução requisito 6.

--- src/zoo.js
+++ src/zoo.js
@@ -38,9 +38,9 @@ function isManager(id) {
   return employees.some((person) => person.managers.includes(id));
 }
 
-// function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-//   // seu código aqui
-// }
+function addEmployee(id, firstName, lastName, [...managers] = [], [...responsibleFor] = []) {
+  return employees.push({ id, firstName, lastName, managers, responsibleFor });
+}
 
 // function animalCount(species) {
 //   // seu código aqui
@@ -78,7 +78,7 @@ module.exports = {
   animalsByIds,
   employeeByName,
   // employeeCoverage,
-  // addEmployee,
+  addEmployee,
   isManager,
   animalsOlderThan,
   // oldestFromFirstSpecies,
