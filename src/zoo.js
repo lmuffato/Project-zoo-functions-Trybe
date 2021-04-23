a4663c3415035abb7e94270126093c52fbd68b87
MZTBTG <mztbtg@hotmail.com>
2021-04-23 18:14:59 -0300

    Willian Zoo Functions

--- src/zoo.js
+++ src/zoo.js
@@ -11,36 +11,57 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-function animalsByIds(ids) {
-  // seu código aqui
+function animalsByIds(...ids) {
+  return data.animals.filter(animal => ids.some(id => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
-  // seu código aqui
+  const result = data.animals.find(especie => especie.name === animal);
+  return result.residents.every(residente => residente.age > age);
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  const dataE = data.employees;
+  const rst = dataE.find(pos => pos.firstName === employeeName || pos.lastName === employeeName);
+  return rst;
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
+  const newEmployee = { ...personalInfo, ...associatedWith };
+  return newEmployee;
 }
 
 function isManager(id) {
-  // seu código aqui
+  function isManegerHere(employe, compareID) {
+    return employe.managers.some(manegerId => manegerId === compareID);
+  }
+  return data.employees.some(employe => isManegerHere(employe, id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor };
+  data.employees.push(newEmployee);
 }
 
 function animalCount(species) {
-  // seu código aqui
 }
 
-function entryCalculator(entrants) {
-  // seu código aqui
+function entryCalculator(entrants = 0) {
+  const pricesData = data.prices;
+  const pricesCategories = Object.keys(pricesData);
+  const totalPrice = pricesCategories.reduce((accumulator, priceCategory) => {
+    const priceToAdd =
+    entrants[priceCategory] ? entrants[priceCategory] * pricesData[priceCategory] : 0;
+    const priceToPay = accumulator + priceToAdd;
+    return priceToPay;
+  }, 0);
+  return totalPrice;
 }
 
 function animalMap(options) {
@@ -48,15 +69,38 @@ function animalMap(options) {
 }
 
 function schedule(dayName) {
-  // seu código aqui
+  const days = {};
+  Object.keys(data.hours).forEach((day) => {
+    if (day !== 'Monday') {
+      days[day] = `Open from ${data.hours[day].open}am until ${(data.hours[day].close) - 12}pm`;
+    } else {
+      days[day] = 'CLOSED';
+    }
+  });
+  if (!dayName) {
+    return days;
+  }
+  return { [dayName]: days[dayName] };
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeData = data.employees.find(employee => employee.id === id);
+  const firstAnimalID = employeeData.responsibleFor[0];
+  const animalResidents = data.animals.find(animal => animal.id === firstAnimalID).residents;
+  const oldestAnimalAge = animalResidents.reduce((accumulator, resident) =>
+    ((resident.age > accumulator) ? resident.age : accumulator), 0);
+  const oldestAnimalResident = animalResidents.find(animal => animal.age === oldestAnimalAge);
+  const { name, sex, age } = oldestAnimalResident;
+  const returnData = [name, sex, age];
+  return returnData;
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
+  const pricesOptions = Object.keys(data.prices);
+  const priceRateToChange = 1 + (percentage / 100);
+  pricesOptions.forEach(priceOption =>
+  (data.prices[priceOption] =
+    (Math.round((data.prices[priceOption] * priceRateToChange) * 100)) / 100));
 }
 
 function employeeCoverage(idOrName) {
@@ -78,3 +122,6 @@ module.exports = {
   increasePrices,
   createEmployee,
 };
+// Playground functions foi legal né? Pra mim, foi.
+// SXNzbyBuw6NvIMOpIHVtYSBzZW5oYS4=
+// 70 61 73 74 65 62 69 6e 2e 63 6f 6d 2f 6b 58 39 79 36 57 55 61
