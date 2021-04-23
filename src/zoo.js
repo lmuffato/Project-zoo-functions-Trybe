e5c4ff493c42d8dba81013514995122a7d270104
MZTBTG <mztbtg@hotmail.com>
2021-04-23 18:32:41 -0300

    Correção Eslint

--- src/zoo.js
+++ src/zoo.js
@@ -12,18 +12,18 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  return data.animals.filter(animal => ids.some(id => animal.id === id));
+  return data.animals.filter((animal) => ids.some((id) => animal.id === id));
 }
 
 function animalsOlderThan(animal, age) {
-  const result = data.animals.find(especie => especie.name === animal);
-  return result.residents.every(residente => residente.age > age);
+  const result = data.animals.find((especie) => especie.name === animal);
+  return result.residents.every((residente) => residente.age > age);
 }
 
 function employeeByName(employeeName) {
   if (!employeeName) return {};
   const dataE = data.employees;
-  const rst = dataE.find(pos => pos.firstName === employeeName || pos.lastName === employeeName);
+  const rst = dataE.find((pos) => pos.firstName === employeeName || pos.lastName === employeeName);
   return rst;
 }
 
@@ -34,9 +34,9 @@ function createEmployee(personalInfo, associatedWith) {
 
 function isManager(id) {
   function isManegerHere(employe, compareID) {
-    return employe.managers.some(manegerId => manegerId === compareID);
+    return employe.managers.some((manegerId) => manegerId === compareID);
   }
-  return data.employees.some(employe => isManegerHere(employe, id));
+  return data.employees.some((employe) => isManegerHere(employe, id));
 }
 
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
@@ -49,24 +49,23 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
   data.employees.push(newEmployee);
 }
 
-function animalCount(species) {
-}
+// function animalCount(species) {
+// }
 
 function entryCalculator(entrants = 0) {
   const pricesData = data.prices;
   const pricesCategories = Object.keys(pricesData);
+  const priceCat = entrants[priceCategory];
   const totalPrice = pricesCategories.reduce((accumulator, priceCategory) => {
-    const priceToAdd =
-    entrants[priceCategory] ? entrants[priceCategory] * pricesData[priceCategory] : 0;
+    const priceToAdd = priceCat ? priceCat * pricesData[priceCategory] : 0;
     const priceToPay = accumulator + priceToAdd;
     return priceToPay;
   }, 0);
   return totalPrice;
 }
 
-function animalMap(options) {
-  // seu código aqui
-}
+// function animalMap(options) {
+// }
 
 function schedule(dayName) {
   const days = {};
@@ -84,12 +83,12 @@ function schedule(dayName) {
 }
 
 function oldestFromFirstSpecies(id) {
-  const employeeData = data.employees.find(employee => employee.id === id);
+  const employeeData = data.employees.find((employee) => employee.id === id);
   const firstAnimalID = employeeData.responsibleFor[0];
-  const animalResidents = data.animals.find(animal => animal.id === firstAnimalID).residents;
+  const animalResidents = data.animals.find((animal) => animal.id === firstAnimalID).residents;
   const oldestAnimalAge = animalResidents.reduce((accumulator, resident) =>
     ((resident.age > accumulator) ? resident.age : accumulator), 0);
-  const oldestAnimalResident = animalResidents.find(animal => animal.age === oldestAnimalAge);
+  const oldestAnimalResident = animalResidents.find((animal) => animal.age === oldestAnimalAge);
   const { name, sex, age } = oldestAnimalResident;
   const returnData = [name, sex, age];
   return returnData;
@@ -97,15 +96,14 @@ function oldestFromFirstSpecies(id) {
 
 function increasePrices(percentage) {
   const pricesOptions = Object.keys(data.prices);
-  const priceRateToChange = 1 + (percentage / 100);
-  pricesOptions.forEach(priceOption =>
-  (data.prices[priceOption] =
-    (Math.round((data.prices[priceOption] * priceRateToChange) * 100)) / 100));
+  const priceToChange = 1 + (percentage / 100);
+  pricesOptions.forEach((priceOption) =>
+    (data.prices[priceOption] = (Math.round((data.prices[priceOption] * priceToChange) * 100)) / 100));
 }
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
+// function employeeCoverage(idOrName) {
+//   // seu código aqui
+// }
 
 module.exports = {
   entryCalculator,
