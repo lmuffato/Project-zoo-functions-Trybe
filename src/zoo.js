f408e600d966880ba69fb9f709235b947778435c
pmaiamateus <pmaiamateus@gmail.com>
2021-04-26 18:00:09 -0300

    adiciona passos 2 3 e 4

--- src/zoo.js
+++ src/zoo.js
@@ -13,24 +13,35 @@ const data = require('./data');
 
 function animalsByIds(...ids) {
   // seu código aqui
+  // procurando is e retornando o id ou vazio
   return (ids !== []) ? ids.map((id) => data.animals.find((animal) => animal.id === id)) : ids;
 }
 
-function animalsOlderThan(animal, age) {
+function animalsOlderThan(animal, ageParameter) {
   // seu código aqui
-  
+  //procurando a espécie
+  const animalspecies = data.animals.find(({name}) => name === animal)
+  //comparando
+  return animalspecies.residents.every(({age}) => age > ageParameter);
 }
 
 function employeeByName(employeeName) {
   // seu código aqui
+  //procurando empregado
+  const employee = data.employees.find(({firstName, lastName}) => firstName === employeeName || lastName === employeeName);
+  // retornando objeto do empregado ou vazio
+  return employee !== undefined ? employee : {};
 }
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
+  // dando spread no conteúdo
+  return { ...personalInfo, ...associatedWith}
 }
 
 function isManager(id) {
   // seu código aqui
+  
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
