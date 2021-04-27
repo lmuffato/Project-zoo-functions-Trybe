cc2cf6229efebfb8335631bd7d2402a1d3d193c9
Moises-Coimbra <moisescoimbra.digitalpro@gmail.com>
2021-04-27 12:35:20 -0300

    Requiremente 5, glory for God

--- src/zoo.js
+++ src/zoo.js
@@ -35,9 +35,9 @@ function employeeByName(name) {
 //   // seu código aqui
 // }
 
-// function isManager(id) {
-//   // seu código aqui
-// }
+function isManager(id) {
+  return employees.some((option) => (option.managers.includes(id)));
+}
 
 // function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 //   // seu código aqui
@@ -80,7 +80,7 @@ module.exports = {
   employeeByName,
   // employeeCoverage,
   // addEmployee,
-  // isManager,
+  isManager,
   // animalsOlderThan,
   // oldestFromFirstSpecies,
   // increasePrices,
