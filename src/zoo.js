627eb593a3c8554896e3a8f811c4450122773406
Wendrick <wendrick.b.c1@gmail.com>
2021-04-28 20:50:30 -0300

    1 requesito de novo por que esqueci do eslint

--- src/zoo.js
+++ src/zoo.js
@@ -14,61 +14,61 @@ const data = require('./data');
 function animalsByIds(...ids) {
   const animalsContainer = [];
   if (ids) {
-    animalsContainer.push(data.animals.find(animal => animal.id === ids));
-    ids.forEach(id => {
-      animalsContainer.push(...data.animals.filter(animal => animal.id === id));
+    animalsContainer.push(data.animals.find((animal) => animal.id === ids));
+    ids.forEach((id) => {
+      animalsContainer.push(...data.animals.filter((animal) => animal.id === id));
     });
   }
   return animalsContainer;
 }
 
-function animalsOlderThan(animal, age) {
+//function animalsOlderThan(animal, age) {
   // seu código aqui
-}
+//}
 
-function employeeByName(employeeName) {
+//function employeeByName(employeeName) {
   // seu código aqui
-}
+//}
 
-function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
-}
+//function createEmployee(personalInfo, associatedWith) {
+//  // seu código aqui
+//}
 
-function isManager(id) {
-  // seu código aqui
-}
+//function isManager(id) {
+//  // seu código aqui
+//}
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
-}
+//function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+//  // seu código aqui
+//}
 
-function animalCount(species) {
-  // seu código aqui
-}
+//function animalCount(species) {
+//  // seu código aqui
+//}
 
-function entryCalculator(entrants) {
-  // seu código aqui
-}
+//function entryCalculator(entrants) {
+//  // seu código aqui
+//}
 
-function animalMap(options) {
-  // seu código aqui
-}
+//function animalMap(options) {
+//  // seu código aqui
+//}
 
-function schedule(dayName) {
-  // seu código aqui
-}
+//function schedule(dayName) {
+//  // seu código aqui
+//}
 
-function oldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+//function oldestFromFirstSpecies(id) {
+//  // seu código aqui
+//}
 
-function increasePrices(percentage) {
-  // seu código aqui
-}
+//function increasePrices(percentage) {
+//  // seu código aqui
+//}
 
-function employeeCoverage(idOrName) {
-  // seu código aqui
-}
+//function employeeCoverage(idOrName) {
+//  // seu código aqui
+//}
 
 module.exports = {
   entryCalculator,
