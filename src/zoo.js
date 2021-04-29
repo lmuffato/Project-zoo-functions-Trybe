33ba9901bf63a4dfac9248af74b9ab140ad59b3d
Wendrick <wendrick.b.c1@gmail.com>
2021-04-28 21:30:22 -0300

    Vendo o progresso

--- src/zoo.js
+++ src/zoo.js
@@ -15,19 +15,32 @@ function animalsByIds(...ids) {
   return data.animals.filter((animal) => ids.find((id) => animal.id === id));
 }
 
-/* function animalsOlderThan(animal, age) {
+function animalsOlderThan(animal, age) {
+  const container = data.animals.find((animalS) => animalS.name === animal)
+  return container.residents.every((animalResident) => animalResident.age > age);
 }
 
 function employeeByName(employeeName) {
+  if (!employeeName) return {};
+  return data.employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
 }
 
 function createEmployee(personalInfo, associatedWith) {
+  const newEmployee = {
+    id: personalInfo.id,
+    firstName: personalInfo.firstName,
+    lastName: personalInfo.lastName,
+    managers: associatedWith.managers,
+    responsibleFor: associatedWith.responsibleFor,
+  };
+  return newEmployee;
 }
 
 function isManager(id) {
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+/* function addEmployee(id, firstName, lastName, managers, responsibleFor) {
 }
 
 function animalCount(species) {
@@ -57,12 +70,12 @@ module.exports = {
 //  animalCount,
 // animalMap,
   animalsByIds,
-//  employeeByName,
+  employeeByName,
 //  employeeCoverage,
 //  addEmployee,
-//  isManager,
-//  animalsOlderThan,
+  isManager,
+  animalsOlderThan,
 //  oldestFromFirstSpecies,
 //  increasePrices,
-//  createEmployee,
+  createEmployee,
 };
