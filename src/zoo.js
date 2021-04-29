2588d6a3dd8c5e3a8b9aba5ec677ab89f282cb45
Wendrick <wendrick.b.c1@gmail.com>
2021-04-28 21:02:57 -0300

    Mais uma chance

--- src/zoo.js
+++ src/zoo.js
@@ -12,64 +12,45 @@ eslint no-unused-vars: [
 const data = require('./data');
 
 function animalsByIds(...ids) {
-  const animalsContainer = [];
-  if (ids) {
-    animalsContainer.push(data.animals.find((animal) => animal.id === ids));
-    ids.forEach((id) => {
-      animalsContainer.push(...data.animals.filter((animal) => animal.id === id));
-    });
-  }
-  return animalsContainer;
+  return data.animals.filter((animal) => ids.find((id) => animal.id === id));
 }
 
-function animalsOlderThan(animal, age) {
-  // seu código aqui
+/* function animalsOlderThan(animal, age) {
 }
 
 function employeeByName(employeeName) {
-  // seu código aqui
 }
 
 function createEmployee(personalInfo, associatedWith) {
-  // seu código aqui
 }
 
 function isManager(id) {
-  // seu código aqui
 }
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
 }
 
 function animalCount(species) {
-  // seu código aqui
 }
 
 function entryCalculator(entrants) {
-  // seu código aqui
 }
 
 function animalMap(options) {
-  // seu código aqui
 }
 
 function schedule(dayName) {
-  // seu código aqui
 }
 
 function oldestFromFirstSpecies(id) {
-  // seu código aqui
 }
 
 function increasePrices(percentage) {
-  // seu código aqui
 }
 
 function employeeCoverage(idOrName) {
-  // seu código aqui
 }
-
+*/
 module.exports = {
   entryCalculator,
   schedule,
