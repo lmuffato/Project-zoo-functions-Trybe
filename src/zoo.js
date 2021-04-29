930553eb0c1d9724cf6a322bff9f2ec0f63d4317
Adriel Falcao Telito <adriel.telito@pampa.compasso>
2021-04-29 03:03:07 -0300

    subindo terceiro exercicio

--- src/zoo.js
+++ src/zoo.js
@@ -47,7 +47,7 @@ function isManager(id) {
 
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
-  const add = []
+  const add = [];
   add.push({ id, firstName, lastName, managers, responsibleFor });
   return add;
 }
