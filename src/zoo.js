30295572f70285d1417ac2c89c9da226b8d01fa1
Adriel Falcao Telito <adriel.telito@pampa.compasso>
2021-04-29 01:04:46 -0300

    subindo primeiro exercicio

--- src/zoo.js
+++ src/zoo.js
@@ -22,6 +22,7 @@ function animalsByIds(...ids) {
   return ani;
 }
 
+/*
 function animalsOlderThan(animal, age) {
   // seu código aqui
 }
@@ -33,55 +34,47 @@ function employeeByName(employeeName) {
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
 }
-
 function isManager(id) {
   // seu código aqui
 }
-
 function addEmployee(id, firstName, lastName, managers, responsibleFor) {
   // seu código aqui
 }
-
 function animalCount(species) {
   // seu código aqui
 }
-
 function entryCalculator(entrants) {
   // seu código aqui
 }
-
 function animalMap(options) {
   // seu código aqui
 }
-
 function schedule(dayName) {
   // seu código aqui
 }
-
 function oldestFromFirstSpecies(id) {
   // seu código aqui
 }
-
 function increasePrices(percentage) {
   // seu código aqui
 }
-
 function employeeCoverage(idOrName) {
   // seu código aqui
 }
+*/
 
 module.exports = {
-  entryCalculator,
-  schedule,
-  animalCount,
-  animalMap,
+  //entryCalculator,
+  //schedule,
+  //animalCount,
+  //animalMap,
   animalsByIds,
-  employeeByName,
-  employeeCoverage,
-  addEmployee,
-  isManager,
-  animalsOlderThan,
-  oldestFromFirstSpecies,
-  increasePrices,
-  createEmployee,
+  //employeeByName,
+  //employeeCoverage,
+  //addEmployee,
+  //isManager,
+  //animalsOlderThan,
+  //oldestFromFirstSpecies,
+  //increasePrices,
+  //createEmployee,
 };
