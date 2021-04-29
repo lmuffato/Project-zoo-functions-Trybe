d8a12fe6916642089d3310d2d5620bce6c5028c4
Adriel Falcao Telito <adriel.telito@pampa.compasso>
2021-04-29 04:02:29 -0300

    subindo terceiro exercicio

--- src/zoo.js
+++ src/zoo.js
@@ -79,9 +79,9 @@ function entryCalculator(entrants) {
   const { Adult, Child, Senior } = data.prices;
   let valor = 0;
   entrants.forEach((entrant) => {
-    entrant.Adult !== undefined ? () => {valor += entrant.Adult * Adult} : valor += 0;
-    entrant.Child !== undefined ? [valor += entrant.Child * Child] : valor += 0;
-    entrant.Senior !== undefined ? [valor += entrant.Senior * Senior] : valor += 0;
+    entrant.Adult !== undefined ? () => { valor += entrant.Adult * Adult } : valor += 0;
+    entrant.Child !== undefined ? () => { valor += entrant.Child * Child } : valor += 0;
+    entrant.Senior !== undefined ? () => { valor += entrant.Senior * Senior } : valor += 0;
   });
   return valor;
 }
